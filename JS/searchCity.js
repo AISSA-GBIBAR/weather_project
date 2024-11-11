7cdcd4b4612bbfcee73b26f8e59d5727dd59c3ad
A-Gbibar <gbibaraissa@gmail.com>
2024-11-11 23:22:37 +0100

    add api city and countries

--- JS/searchCity.js
+++ JS/searchCity.js
@@ -1,31 +1,35 @@
 var config = {
-    CountriesUrl : "https://api.countrystatecity.in/v1/countries", 
-    Countrieskey : "SHRsRUJsOGRRMDcwZlQ0bGxsZXJaQ1V3bTN2eWJBNlZiYW1CTGpCeA=="
-}
+	CountriesUrl: "https://api.countrystatecity.in/v1/countries",
+	Countrieskey: "SHRsRUJsOGRRMDcwZlQ0bGxsZXJaQ1V3bTN2eWJBNlZiYW1CTGpCeA==",
+};
 
 var name_city = document.querySelector(".name_city");
 
+var all_city_of_world = {};
+
 function loadCountries() {
-    let apiEndpoint = config.CountriesUrl;
+	let apiEndpoint = config.CountriesUrl;
 
-    fetch(apiEndpoint, {headers: {"X-CSCAPI-KEY": config.Countrieskey}})
-    .then(Response => Response.json())
-    .then(data => {
-        data.forEach(country =>{
-            
-            let shortCut_contry = country.iso2;
-            let urlCity = `${apiEndpoint}/${shortCut_contry}/cities`;
-
-            fetch(urlCity, {headers: {"X-CSCAPI-KEY": config.Countrieskey}})
-            .then(Response => Response.json())
-            .then(data => {
-                 console.log(data)
-             })
-        })
-
-
-    })
-    .catch(error => console.error('Error loading countries : ', error ))
+	fetch(apiEndpoint, { headers: { "X-CSCAPI-KEY": config.Countrieskey } })
+		.then((Response) => Response.json())
+		.then((data) => {
+			data.forEach((country) => {
+				let shortCut_contry = country.iso2;
+				// let urlCity = `${apiEndpoint}/${shortCut_contry}/cities`;
+				all_city_of_world[country.name] = [shortCut_contry];
+			});
+		})
+		.catch((error) => console.error("Error loading countries : ", error));
 }
+loadCountries() 
 
-loadCountries()
+function loadCity() {
+	let apiEndpoint = config.CityUrl;
+	for (let key in all_city_of_world) {
+		console.log(key);
+        
+	}
+    // console.log(all_city_of_world);
+    
+}
+loadCity();
